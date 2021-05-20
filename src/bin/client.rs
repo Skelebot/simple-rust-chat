a460aeffac46aef2aa7155b2a323011cdfcdfcd0
Skelebot <antonisimka.8@gmail.com>
2021-05-20 09:35:45 +0200

    Initial commit

---
+++ src/bin/client.rs
@@ -0,0 +1,104 @@
+use std::{io::prelude::*, sync::mpsc::TryRecvError};
+use std::net;
+use std::{io, sync::mpsc, thread};
+
+const ERR: u8 = 0xFF;
+const _OK: u8 = 0x01;
+
+fn main() -> anyhow::Result<()> {
+    let (ip, nickname) = parse_args()?;
+
+    let mut stream = net::TcpStream::connect(ip)?;
+    let mut reader = io::BufReader::new(stream.try_clone()?);
+
+    let mut srv_name = String::new();
+    reader.read_line(&mut srv_name)?;
+    srv_name.pop(); // Pop the newline
+    println!("Connected to {}!", srv_name);
+    print!("> ");
+    std::io::stdout().flush()?;
+
+    // Write our nickname
+    stream.write(nickname.as_bytes())?;
+    stream.write(b"\n")?;
+
+    // Check for errors
+    let mut buf = [0; 1];
+    if stream.read(&mut buf)? == 1 {
+        if buf[0] == ERR {
+            let mut msg = String::new();
+            reader.read_line(&mut msg)?;
+            println!(" Server Error: {}", msg);
+            return Err(anyhow::anyhow!("Server returned an error"))
+        }
+    }
+
+    let stdin_channel = spawn_stdin_channel();
+    let stream_channel = spawn_stream_channel(stream.try_clone()?);
+
+    loop {
+        if let Some(msg) = stdin_channel.try_recv().ok() {
+            match msg.as_str() {
+                "exit\n" => {
+                    stream.write(b"disconnect\n")?;
+                    println!("\nDisconnected!");
+                    break;
+                }
+                s => {
+                    stream.write(s.as_bytes())?;
+                    print!("> ");
+                    std::io::stdout().flush()?;
+                }
+            }
+        }
+        match stream_channel.try_recv() {
+            Ok(msg) => match msg.as_str() {
+                "exit\n" => break,
+                s => {
+                    print!("\r< {}> ", s);
+                    std::io::stdout().flush()?;
+                }
+            }
+            Err(TryRecvError::Disconnected) => {
+                println!("\nServer disconnected.");
+                break;
+            }
+            _ => (),
+        }
+    }
+
+    Ok(())
+}
+
+fn spawn_stdin_channel() -> mpsc::Receiver<String> {
+    let (tx, rx) = mpsc::channel::<String>();
+    thread::spawn(move || loop {
+        let mut buffer = String::new();
+        io::stdin().read_line(&mut buffer).unwrap();
+        tx.send(buffer).unwrap();
+    });
+    rx
+}
+
+fn spawn_stream_channel(stream: net::TcpStream) -> mpsc::Receiver<String> {
+    let (tx, rx) = mpsc::channel::<String>();
+    let mut bufread = io::BufReader::new(stream);
+    thread::spawn(move || loop {
+        let mut buffer = String::new();
+        if bufread.read_line(&mut buffer).unwrap() == 0 {
+            return;
+        }
+        tx.send(buffer).unwrap();
+    });
+    rx
+}
+
+fn parse_args() -> anyhow::Result<(String, String)> {
+    match std::env::args().count() {
+        3 => Ok((
+            std::env::args().nth(1).unwrap(),
+            std::env::args().nth(2).unwrap(),
+        )),
+        _ => Err(anyhow::anyhow!("Expected 2 arguments: IP, client name")),
+    }
+}
---
+++ src/bin/server.rs
@@ -0,0 +1,144 @@
+use std::{
+    io::prelude::*,
+    sync::{Arc, Mutex, mpsc},
+    collections::HashMap,
+    io, net,
+};
+
+const ERR: u8 = 0xff;
+const OK: u8 = 0x01;
+
+type UsersList = Arc<Mutex<HashMap<Arc<String>, net::TcpStream>>>;
+
+fn main() -> anyhow::Result<()> {
+    let (socket, srv_name) = parse_args()?;
+    let listener = net::TcpListener::bind(socket)?;
+
+    // Client threads send messages through this channel to the broadcast thread
+    let (msg_tx, msg_rx) = mpsc::channel::<Message>();
+
+    let users: UsersList = Arc::new(Mutex::new(HashMap::new()));
+
+    // Spawn the thread that broadcasts messages to connected clients
+    {
+        let handle = users.clone();
+        std::thread::spawn(move || serve_messages(msg_rx, handle));
+    }
+
+    loop {
+        let (mut stream, addr) = listener.accept()?;
+
+        // Send server name
+        stream.write(srv_name.as_bytes())?;
+        stream.write(b"\n")?;
+
+        // Receive the client's nickname
+        let nickname = {
+            let mut reader = io::BufReader::new(stream.try_clone().unwrap());
+            let mut client_name = String::new();
+            reader.read_line(&mut client_name)?;
+            client_name.pop();
+            Arc::new(client_name)
+        };
+
+        let tx = msg_tx.clone();
+        // Check if the nickname is available
+        {
+            let mut users = users.lock().unwrap();
+            if users.contains_key(&*nickname) {
+                stream.write(&[ERR])?;
+                stream.write(b"Nickname already taken!\n")?;
+            } else {
+                stream.write(&[OK])?;
+                users.insert(nickname.clone(), stream.try_clone().unwrap());
+                tx.send(Message::Connect(nickname.clone(), addr))?;
+                std::thread::spawn(move || handle_client(stream, tx, nickname.clone()));
+            }
+        }
+    }
+}
+
+#[derive(Debug)]
+enum Message {
+    Connect(Arc<String>, net::SocketAddr),
+    Disconnect(Arc<String>),
+    Text(Arc<String>, String),
+}
+
+fn handle_client(
+    stream: net::TcpStream,
+    tx: mpsc::Sender<Message>,
+    nick: Arc<String>,
+) -> anyhow::Result<()> {
+    let mut reader = io::BufReader::new(stream.try_clone().unwrap());
+
+    loop {
+        let mut msg = String::new();
+        if reader.read_line(&mut msg)? == 0 {
+            tx.send(Message::Disconnect(nick.clone()))?;
+            return Ok(())
+        }
+        match msg.as_str() {
+            "disconnect\n" => break,
+            text if msg.len() > 1 => {
+                tx.send(Message::Text(nick.clone(), text.to_string()))?;
+            }
+            _ => {}
+        }
+    }
+
+    tx.send(Message::Disconnect(nick.clone()))?;
+    std::io::stdout().flush().unwrap();
+    Ok(())
+}
+
+fn serve_messages(rx: mpsc::Receiver<Message>, users: UsersList) -> anyhow::Result<()> {
+    loop {
+        let msg = rx.recv()?;
+        let mut users = users.lock().unwrap();
+        match msg {
+            Message::Connect(nick, addr) => {
+                println!("User {} connected from ip: {}!", nick, addr);
+                for (uid, user) in users.iter_mut() {
+                    if *uid != nick {
+                        user.write_fmt(format_args!("User {} connected from ip: {}!\n", nick, addr))?;
+                    }
+                }
+            }
+            Message::Disconnect(nick) => {
+                println!("User {} disconnected.", nick);
+                // Properly disconnect the user
+                let u = users.get_mut(&nick).unwrap();
+                u.flush()?;
+                u.shutdown(net::Shutdown::Both)?;
+                users.remove(&*nick);
+                for (uid, user) in users.iter_mut() {
+                    if *uid != nick {
+                        user.write_fmt(format_args!("User {} disconnected.\n", nick))?;
+                    }
+                }
+            }
+            Message::Text(nick, text) => {
+                print!("{}: {}", nick, text);
+                for (uid, user) in users.iter_mut() {
+                    if *uid != nick {
+                        user.write_fmt(
+                            format_args!("{}: {}", nick, text)
+                        )?;
+                    }
+                }
+            }
+        }
+        std::io::stdout().flush().unwrap();
+    }
+}
+
+fn parse_args() -> anyhow::Result<(String, String)> {
+    use std::env::args;
+    match args().count() {
+        3 => Ok((args().nth(1).unwrap(), args().nth(2).unwrap())),
+        _ => Err(anyhow::anyhow!(
+            "Expected 2 arguments: socket address, server name"
+        )),
+    }
+}
